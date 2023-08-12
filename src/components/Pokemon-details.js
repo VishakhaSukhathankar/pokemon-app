34bbba6c5d107da54c5a590e50104b23427c196d
Vishakha <vishakha.sukhathankar@newfold.com>
2023-08-13 01:12:17 +0530

    added left section and optimised code

--- src/components/Pokemon-details.js
+++ src/components/Pokemon-details.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Paper, Grid } from '@mui/material';
+import { Box, Paper, Grid, Card, CardMedia, CardContent, SvgIcon, Button } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { getCachedData, openDB, cacheData} from "../utils/helpers/IndexedDBUtility";
 import { styled } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
 import EvoultionChain from './evolution/evolution-chain';
 import "./styles.scss";
+import LinearProgress, {
+  linearProgressClasses,
+} from "@mui/material/LinearProgress";
+import { ArrowLeft, ArrowRight, Weight, Height } from "../assets";
+import PokemonCard from './dashboard/Pokemon-card';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -14,6 +19,21 @@ const Item = styled(Paper)(({ theme }) => ({
     textAlign: 'center',
     color: theme.palette.text.secondary,
 }));
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+    height: 5,
+    width: 120,
+    borderRadius: 5,
+    marginTop: 6,
+    [`&.${linearProgressClasses.colorPrimary}`]: {
+      backgroundColor:
+        theme.palette.grey[theme.palette.mode === "light" ? 200 : 800],
+    },
+    [`& .${linearProgressClasses.bar}`]: {
+      borderRadius: 5,
+      backgroundColor: theme.palette.mode === "light" ? "#EE3F3E" : "#EE3F3E",
+    },
+  }));
 
 const PokemonDetails = () => {
     const { name } = useParams();
@@ -72,14 +92,40 @@ const PokemonDetails = () => {
         return result;
       }
     const allSpeciesNames = extractSpeciesNames(evolutionChain?.chain);
+    const statsDetails = combinePokemonDetailsData?.stats.map((val, ind) => val.stat.name)
+    console.log(statsDetails, "statsDetails")
+    console.log(combinePokemonDetailsData, "combinePokemonDetailsData")
     return (combinePokemonDetailsData ? (
         <section>
             <Box sx={{ flexGrow: 1 }}>
-                <Grid container spacing={2}>
-                    <Grid item xs={4}>
-                        <Item>xs=8</Item>
+                <Grid container spacing={2} columns={16}>
+                    <Grid item xs={16} sm={8} md={4} lg={4} xl={4}>
+                        <Card>
+                            <PokemonCard pokemonName={combinePokemonDetailsData?.name} classIdentifier="detail-section-left"/>
+                            <div className="pagination">
+                                <Button
+                                    // disabled={pageNumber <= 0}
+                                    // onClick={handlePrevClick}
+                                    variant="contained"
+                                >
+                                    <span className="arw-left">
+                                    <ArrowLeft />
+                                    </span>{" "}
+                                    Prev
+                                </Button>
+                                <Button
+                                    // onClick={handleNextClick}
+                                    variant="contained"
+                                >
+                                    Next{" "}
+                                    <span className="arw-right">
+                                    <ArrowRight />
+                                    </span>
+                                </Button>
+                            </div>
+                        </Card>
                     </Grid>
-                    <Grid item xs={8}>
+                    <Grid item xs={16} sm={8} md={12} lg={12} xl={12}>
                         <Item className='pokemon-details'>
                             <div className='detail-wrap'>
                                     <h3 className='detail-title'>Versions</h3>
--- src/components/dashboard/Pokemon-card.js
+++ src/components/dashboard/Pokemon-card.js
@@ -1,11 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardMedia, CardContent, SvgIcon } from '@mui/material';
+import { Card, CardMedia, CardContent, SvgIcon, Grid } from '@mui/material';
 import { Weight, Height } from "../../assets";
 import { Link } from 'react-router-dom';
 import { openDB, cacheData, getCachedData } from "../../utils/helpers/IndexedDBUtility";
+import LinearProgress, {
+    linearProgressClasses,
+  } from "@mui/material/LinearProgress";
+  import { styled } from '@mui/material/styles';
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+    height: 5,
+    width: 120,
+    borderRadius: 5,
+    marginTop: 6,
+    [`&.${linearProgressClasses.colorPrimary}`]: {
+      backgroundColor:
+        theme.palette.grey[theme.palette.mode === "light" ? 200 : 800],
+    },
+    [`& .${linearProgressClasses.bar}`]: {
+      borderRadius: 5,
+      backgroundColor: theme.palette.mode === "light" ? "#EE3F3E" : "#EE3F3E",
+    },
+  }));
 
 const PokemonCard = (props) => {
-    const { pokemonName, evolutionClass } = props;
+    const { pokemonName, classIdentifier } = props;
     const [combinePokemonDetailsData, setcombinePokemonDetailsData] = useState(null);
 
     const combinePokemonData = () => {
@@ -40,31 +59,51 @@ const PokemonCard = (props) => {
     useEffect(() => {
         combinePokemonData();
     }, [pokemonName]);
-
-    if (!combinePokemonDetailsData) {
-        return <p>Loading...</p>;
-    }
-    const badge = Object.values(combinePokemonDetailsData?.types)?.map(_ => _.type.name);
+    const badge = combinePokemonDetailsData && Object.values(combinePokemonDetailsData.types).map(_ => _.type.name);
     const pokemonDescription = combinePokemonDetailsData?.flavor_text_entries?.find(entry => entry.language.name === "en")?.flavor_text || "";
-    return (
-        <Card sx={{ height: 370 }} className={evolutionClass}>
-            <Link to={`/pokemon/${combinePokemonDetailsData?.name}`}>
-                {evolutionClass === "evolution-chain" ? (
-                    <CardContent className="evolution-wrap" sx={{ width: 128, height: 128, margin: "0 auto" }}>
+    return (combinePokemonDetailsData ? (
+        <Card sx={{ height: classIdentifier !== "detail-section-left" ? 370 : "auto" }} className={classIdentifier}>
+        <Link to={`/pokemon/${combinePokemonDetailsData?.name}`}>
+            {classIdentifier === "evolution-chain" ? (
+                <CardContent className="evolution-wrap" sx={{ width: 128, height: 128, margin: "0 auto" }} >
+                    <CardMedia
+                        sx={{ maxWidth: "100%", width: "auto", height: "100%", margin: "0 auto" }}
+                        component="img"
+                        image={combinePokemonDetailsData?.sprites?.other?.dream_world?.front_default}
+                        alt={pokemonName}
+                    />
+                    <p className='name'>{ combinePokemonDetailsData?.name }</p>
+                </CardContent>
+            ) : (
+                <CardContent>
+                    <div className='header-wrap'>
+                        <div className='badge'>
+                            {badge.map((name, ind) => <span key={ind} className={ `type-${name}`}>{ name }</span>)}
+                        </div>
+                        {classIdentifier !== "detail-section-left" && <>
+                            <div className='height'>
+                                <SvgIcon aria-label="height" sx={{ padding: 0 }}>
+                                    <Height sx={{ height: 38, width: 38 }} />
+                                </SvgIcon>
+                                <span>{combinePokemonDetailsData?.height} M</span>
+                            </div>
+                            <div className='weight'>
+                                <SvgIcon aria-label="weight" sx={{ padding: 0 }}>
+                                    <Weight sx={{ height: 38, width: 38 }} />
+                                </SvgIcon>
+                                <span>{combinePokemonDetailsData?.weight} LBS</span>
+                            </div>
+                        </>}
+                    </div>
+                    <CardContent sx={{ width: 182, height: 182, margin: "0 auto" }}>
                         <CardMedia
                             sx={{ maxWidth: "100%", width: "auto", height: "100%", margin: "0 auto" }}
                             component="img"
                             image={combinePokemonDetailsData?.sprites?.other?.dream_world?.front_default}
                             alt={pokemonName}
                         />
-                        <p className='name'>{ combinePokemonDetailsData?.name }</p>
                     </CardContent>
-                ) : (
-                    <CardContent>
-                    <div className='header-wrap'>
-                        <div className='badge'>
-                                {badge.map((name, ind) => <span key={ind} className={ `type-${name}`}>{ name }</span>)}
-                        </div>
+                    {classIdentifier === "detail-section-left" && <div className='section-left-header-wrap'>
                         <div className='height'>
                             <SvgIcon aria-label="height" sx={{ padding: 0 }}>
                                 <Height sx={{ height: 38, width: 38 }} />
@@ -77,22 +116,38 @@ const PokemonCard = (props) => {
                             </SvgIcon>
                             <span>{combinePokemonDetailsData?.weight} LBS</span>
                         </div>
-                    </div>
-                    <CardContent sx={{ width: 182, height: 182, margin: "0 auto" }}>
-                        <CardMedia
-                            sx={{ maxWidth: "100%", width: "auto", height: "100%", margin: "0 auto" }}
-                            component="img"
-                            image={combinePokemonDetailsData?.sprites?.other?.dream_world?.front_default}
-                            alt={pokemonName}
-                        />
-                    </CardContent>
-                    <p className='name'>{ combinePokemonDetailsData?.name }</p>
-                    <p className='description'>{ pokemonDescription }</p>
+                    </div>}
+                    <p className='name'>{combinePokemonDetailsData?.name}</p>
+                    {classIdentifier !== "detail-section-left" && <p className='description'>{pokemonDescription}</p>}
+                    <Grid
+                        container
+                        spacing={4}
+                        columns={12}
+                        className="flex justify-center items-center p-8"
+                    >
+                        {combinePokemonDetailsData?.stats.map((val, ind) => {
+                            const statName = val.stat.name === "special-attack" ? "Sp.Attack" : val.stat.name === "special-defense" ? "Sp.Defense" : val.stat.name;
+                            return(                                
+                                <Grid item xs={6} md={6} lg={6} xl={6} key={ind}>
+                                    <div className='progress-textwrap'>
+                                        <span className="float-left stat-name">{statName}</span>
+                                        <div className="details-weight stat-val">
+                                            <span>{val.base_stat}</span>
+                                        </div>
+                                    </div>
+                                    <BorderLinearProgress variant="determinate" value={val.base_stat * 100 / 255} maxValue={100} />
+                                </Grid>
+                            )
+                        })}
+                    </Grid>
                 </CardContent>
-                )}
-                
-            </Link>
-        </Card>
+            )}
+            
+        </Link>
+    </Card>
+    ) : (
+            <p>Loading...</p>
+        )        
     );
 }
 
